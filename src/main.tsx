febfeab710cf699dbba03c40ebd63500a17db98f
ronyislam502 <ronyislam502@gmail.com>
2025-01-13 00:02:46 +0600

    redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routers.tsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <div className="max-w-7xl mx-auto"> */}
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
-    {/* // </div> */}
+    <Toaster />
   </React.StrictMode>
 );
